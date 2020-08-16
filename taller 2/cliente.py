372f92c284ddec89b7fcf08dc76f519e34b257d4
BogoCoder <samuelperez.di@gmail.com>
2020-08-15 21:34:33 -0500

    cliente y saldo

---
+++ taller 2/cliente.py
@@ -0,0 +1,40 @@
+from socket import *
+from time import sleep
+
+servidorNombre = "127.0.0.1" 
+servidorPuerto = 12000
+
+def solicitar(tipoSolicitud, cantidad):
+    clienteSocket = socket(AF_INET, SOCK_STREAM)
+    clienteSocket.connect((servidorNombre,servidorPuerto))
+    if tipoSolicitud == 2 or tipoSolicitud == 3:
+        solicitud = str(tipoSolicitud)+str(cantidad)
+    else:
+        solicitud = str(tipoSolicitud)
+    clienteSocket.send(bytes(solicitud, "utf-8"))
+    respuesta = clienteSocket.recv(1024)
+    print(str(respuesta, "utf-8"))
+    clienteSocket.close()
+
+print("Solicitar el saldo al servidor.")
+solicitar(1,0)
+sleep(1)
+
+print("Debitar una cantidad superior al saldo.")
+solicitar(2,6000)
+sleep(1)
+
+print("Debitar una cantidad inferior al saldo.")
+solicitar(2,1000)
+sleep(1)
+
+print("Solicitar el nuevo saldo.")
+solicitar(1,0)
+sleep(1)
+
+print("Acreditar el saldo para duplicarlo.")
+solicitar(3,4000)
+sleep(1)
+
+print("Acreditar el nuevo saldo.")
+solicitar(1,0)
